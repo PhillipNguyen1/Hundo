6faf933697388b83c72a9010dd04160a373b0a14
TheZebull <nicholas.ghansyam@gmail.com>
2018-09-25 19:12:30 -0500

    im N IDIOT

--- Hundo/Assets/PlayerMovement.cs
+++ Hundo/Assets/PlayerMovement.cs
@@ -2,41 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-    public float movementSpeed;
+    Rigidbody2D body;
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+    public float runSpeed = 4;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Movement();
-	}
-
-    void Movement() {
-
-        if (Input.GetKey(KeyCode.D)) {
-            transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Translate(Vector2.left * movementSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.W)) {
-            transform.Translate(Vector2.up * movementSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.S)) {
-            transform.Translate(Vector2.down * movementSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
     }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0)
+            body.velocity = new Vector2((horizontal * runSpeed) * moveLimiter, (vertical * runSpeed) * moveLimiter);
+        else
+            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
 }
