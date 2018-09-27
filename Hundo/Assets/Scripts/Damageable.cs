7cf7e1fcb492d974f8070be212c33c1c98dd8f69
Chris Heuer <chrisdheuer@gmail.com>
2018-09-26 22:36:05 -0500

    :'(

---
+++ Hundo/Assets/Scripts/Damageable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Damageable : MonoBehaviour {
+
+	public enum Status { ACTIVE, DESTROYED };
+
+	public float HP;
+	public Status status;
+
+	protected abstract void OnHit(float damage);
+	
+	protected abstract void OnDestroyed();
+
+	public void Hit(float damage) {
+		if (status == Status.ACTIVE) {
+			OnHit(damage);
+
+			if (HP <= 0.0f) {
+				OnDestroyed();
+				status = Status.DESTROYED;
+			}
+		}
+	}
+}
---
+++ Hundo/Assets/Scripts/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour {
+
+	private bool isReady;
+	public float Rate;
+
+	public Weapon() {
+		isReady = true;
+	}
+
+	protected abstract void OnUse();
+
+	public bool Use() {
+		if (isReady) {
+			StartCoroutine(exec());
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private IEnumerator exec() {
+		isReady = false;
+		OnUse();
+		yield return new WaitForSeconds(Rate);
+		isReady = true;
+	}
+}
